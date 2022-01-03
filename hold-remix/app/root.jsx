fcbad1704ac9f809157199edadea073151a18a3d
Your Name <angelicrf@gmail.com>
2022-01-03 11:44:31 -0800

    initial app

---
+++ hold-remix/app/root.jsx
@@ -0,0 +1,31 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "remix";
+
+export function meta() {
+  return { title: "Angelique-Remix-app" };
+}
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", backgroundColor: "darkviolet" }}>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
+}
---
+++ hold-remix/app/routes/index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", backgroundColor: "blue" }}>
+      <h1>Angelique Remix-App</h1>
+    </div>
+  );
+}
