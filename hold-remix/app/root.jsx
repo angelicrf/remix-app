76348b7420dbecd4cd63aa7f857b646cd9d351b2
Your Name <angelicrf@gmail.com>
2022-01-03 13:06:33 -0800

    colors-updated

--- hold-remix/app/root.jsx
+++ hold-remix/app/root.jsx
@@ -10,7 +10,16 @@ import {
 export function meta() {
   return { title: "Angelique-Remix-app" };
 }
-
+const allColors = {
+  "app-blue": "#1E90FF",
+  "app-red": "#8B0000",
+  "app-gray": "#A9A9A9",
+  "app-yellow": "#FFF8DC",
+  "app-violet": "#9932CC",
+  "app-cyan": "#008B8B",
+  "app-white": "#F8F8FF",
+  "app-lavendar" : "#FFF0F5"
+}
 export default function App() {
   return (
     <html lang="en">
@@ -20,7 +29,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", backgroundColor: "darkviolet" }}>
+      <body style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", backgroundColor: allColors["app-lavendar"] }}>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- hold-remix/app/routes/index.jsx
+++ hold-remix/app/routes/index.jsx
@@ -1,6 +1,16 @@
+const allTxtColors = {
+  "app-blue": "#1E90FF",
+  "app-red": "#8B0000",
+  "app-gray": "#A9A9A9",
+  "app-yellow": "#FFF8DC",
+  "app-violet": "#9932CC",
+  "app-cyan": "#008B8B",
+  "app-white": "#F8F8FF",
+  "app-lavendar" : "#FFF0F5"
+}
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", backgroundColor: "blue" }}>
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4", backgroundColor: allTxtColors["app-blue"] }}>
       <h1>Angelique Remix-App</h1>
     </div>
   );
